303c3db0b498324286d7614ad1e7ecb9dfaf4ba0
nikit34 <permikov134@mail.ru>
2023-11-13 22:14:58 +0000

    Refactoring

---
+++ my.py
@@ -0,0 +1,168 @@
+from PIL import Image
+import numpy as np
+import pyprind
+import random
+import os
+import sys
+import cv2
+import pygame
+from collections import defaultdict, Counter
+
+
+class MarkChain:
+    def __init__(self, bucket_size = 10, four_nb = True):
+        self.weights = defaultdict(Counter)
+        self.bucket_size = bucket_size
+        self.four_nb = four_nb
+
+    def norm(self, pixel):
+        return pixel
+
+    def denomalize(self, pixel):
+        return pixel * self.bucket_size
+
+    def get_neighbs(self, x, y):
+        if self.four_nb:
+            return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        else:
+            return [
+                (x + 1, y),
+                (x - 1, y),
+                (x, y + 1),
+                (x, y - 1),
+                (x + 1, y + 1),
+                (x - 1, y - 1),
+                (x - 1, y + 1),
+                (x + 1, y - 1)
+            ]
+
+    def get_neighbours_dir(self, x, y):
+        if self.four_neighbour:
+            return {'r': (x + 1, y), 'l': (x - 1, y), 'b': (x, y + 1), 't': (x, y - 1)}
+        else:
+            return dict(zip(
+                ['r', 'l', 'b', 't', 'br', 'tl', 'tr', 'bl'],
+                [
+                    (x + 1, y),
+                    (x - 1, y),
+                    (x, y + 1),
+                    (x, y - 1),
+                    (x + 1, y + 1),
+                    (x - 1, y - 1),
+                    (x - 1, y + 1),
+                    (x + 1, y - 1)
+                ]
+            ))
+
+    def train(self, img):
+        width, height = img.size
+        img = np.array(img)[:, :, :3]
+        prog = pyprind.ProgBar((width * height), width = 64, stream = 1)
+
+        for x in range(height):
+            for y in range(height):
+                pix = tuple(self.norm(ing[x, y]))
+                prog.update()
+                for neighb in self.get_neighbs(x, y):
+                    try:
+                        self.weights[pix][tuple(self.norm(img[neighb]))] += 1
+                    except IndexError:
+                        continue
+        self.directional = False
+
+
+    def gen(self, init_state = None, width = 512, height = 512):
+        fourcc = cv2.VideoWriter_fourcc(*'MP4v')
+        writer = cv2.VideoWriter('markov_img.mp4', fourcc, 24, (width, height))
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+        pygame.init()
+
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Markov Image')
+        screen.fill((0, 0, 0))
+
+        if init_state is None:
+            init_state = random.choice(list(self.weights.keys()))
+        if type(init_state) is not tuple and len(init_state) != 3:
+            raise ValueError("Initial State must be a 3-tuple")
+        img = Image.new('RGB', (width, height), 'white')
+        img = np.array(img)
+        img_out = np.array(img.copy())
+
+        init_pos = (np.random.randint(0, width), np.random.randint(0, height))
+        img[init_pos] = init_state
+        stack = [init_pos]
+        coloured = set()
+        i = 0
+        prog = pyprind.ProgBar((width * height), width=64, stream=1)
+        while stack:
+            x, y = stack.pop()
+            if (x, y) in coloured:
+                continue
+            else:
+                coloured.add((x, y))
+            try:
+                cpixel = img[x, y]
+                node = self.weights[tuple(cpixel)]
+                img_out[x, y] = self.denormalize(cpixel)
+                prog.update()
+                i += 1
+                screen.set_at((x, y), img_out[x, y])
+                if i % 128 == 0:
+                    pygame.display.flip()
+                    pass
+            except IndexError:
+                continue
+
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    sys.exit()
+
+            if self.directional:
+                keys = {dir: list(node[dir].keys()) for dir in node}
+                neightbours = self.get_neightbours_dir(x, y).items()
+                counts = {dir:np.arange(len(node[dir])) for dir in keys}
+                ps = {dir: counts[dir] / counts[dir].sum() for dir in keys}
+            else:
+                keys = list(node.keys())
+                heighbours = self.get_neighbours(x, y)
+                counts = np.array(list(node.values()), dtype = np.float32)
+                key_idxs = np.arange(len(keys))
+                ps = counts / counts.sum()
+            np.randem.shuffle(neighbours)
+            for neightbour in neightbours:
+                try:
+                    if self.directional:
+                        direction = neighbour[0]
+                        neighbour = neighbour[1]
+                        if neighbour not in coloured:
+                            col_idx = np.random.choice(key_idxs[direction], p = ps[direction])
+                            img[neighbour] = keys[direction][col_idx]
+                        else:
+                            col_idx = np.random.choice(key_idxs, p = ps)
+                            if neighbour not in coloured:
+                                img[neighbour] = keys[col_idx]
+                except IndexError:
+                    pass
+                except ValueError:
+                    continue
+                if 0 <= neighbour[0] < width and 0 <= neighbour[1] < height:
+                    stack.append(neighbour)
+        writer.release()
+        return Image.fromarray(img_out)
+
+
+if __name__  == "__main__":
+    chain = MarkChain(bucket_size = 16, four_nb = True)
+    try:
+        fnames = sys.argv[1:]
+    except IndexError:
+        fnames = ['АВА.jpg']
+    for fname in fnames:
+        im = Image.open(fname)
+        im.show()
+        print("Training " + fname)
+        chain.train(im)
+    print(chain.weights)
+    print("\nGenerating")
+    chain.gen().show()
